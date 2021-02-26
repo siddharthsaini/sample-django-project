ee270d77791236a7cfdcaec392f8312d454609ba
sidxharth <sidxharth@gmail.com>
2021-02-26 17:08:10 +0530

    initial commit - base project

---
+++ apis/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+# models are essentially, your database layout, with additional metadata.
+# generally each model maps to a single database table
